718644b538a960f1c1f64f863464f1a3939b16cb
TommYs00 <102375117+TommYs00@users.noreply.github.com>
2022-06-17 00:04:32 +0200

    corrected default units in all <p> tags which have class 'conversion-p' ; finished js file, the program is fully usable

--- index.js
+++ index.js
@@ -12,7 +12,20 @@ const conversionVolume = document.getElementById("volume");
 const conversionMass = document.getElementById("mass");
 
 convertButton.addEventListener("click", function(){
-    let userValue = userInput.value;
-    console.log(typeof userValue)
-})
+    let userValue = Number(userInput.value);
+    if (userValue !== NaN) {
 
+        let metersToFeet = (userValue * 3.281).toFixed(3);
+        let feetToMeters = (userValue / 3.281).toFixed(3);
+
+        let litersToGallons = (userValue * 0.264).toFixed(3);
+        let gallonsToLiters = (userValue / 0.264).toFixed(3);
+
+        let kilogramsToPounds = (userValue * 2.204).toFixed(3);
+        let poundsToKilograms = (userValue / 2.204).toFixed(3);
+
+        conversionLength.textContent = `${userValue} meters = ${metersToFeet} feet | ${userValue} feet = ${feetToMeters} meters`
+        conversionVolume.textContent = `${userValue} liters = ${litersToGallons} gallons | ${userValue} gallons = ${gallonsToLiters} liters`
+        conversionMass.textContent = `${userValue} kilos = ${kilogramsToPounds} pounds | ${userValue} pounds = ${poundsToKilograms} kilos`
+    }
+})
